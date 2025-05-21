9c2beba41a77d4c95a53b2cc97f4dd01042102d4
Kerboul <31707223+DaKerboul@users.noreply.github.com>
2025-05-21 22:42:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
-cd .. && ./update_git.sh
+cd ..
+./update_git.sh
+cd front_end/quizine
+ls -l
+docker compose up -d --build
+cd ../..
+cd backend
+ls -l
+docker compose up -d --build
+cd ..
+cd build-deploy
+ls -l
+echo "Deploy Ended"
