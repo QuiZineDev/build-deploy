1e202a7346ca1fba5c81f77f997fb87dad01ab1f
root <root@quiz-server.reborn.gov>
2025-05-23 07:37:22 +0200

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,17 @@ cd ..
 ./update_git.sh
 cd front_end/quizine
 ls -l
-docker compose up -d --build
+docker compose down
+docker volume prune
+docker compose build --no-cache
+docker compose up -d
 cd ../..
 cd backend
 ls -l
-docker compose up -d --build
+docker compose down
+docker volume prune
+docker compose build --no-cache
+docker compose up -d
 cd ..
 cd build-deploy
 ls -l
